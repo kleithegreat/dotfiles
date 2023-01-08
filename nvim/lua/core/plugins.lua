2e9de678c1f8720aaafd5b2b2994ad9a35b5a4c3
Kevin Lei <kevvinlei89@gmail.com>
2023-01-08 17:01:12 -0500

    neovim and hyprland configs

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -29,8 +29,17 @@ return require('packer').startup(function(use)
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
   }
+
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
+
+  use {
+    "windwp/nvim-autopairs",
+      config = function() require("nvim-autopairs").setup {} end
+  }
+
+  use 'Pocco81/auto-save.nvim'
+
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use "rafamadriz/friendly-snippets"
