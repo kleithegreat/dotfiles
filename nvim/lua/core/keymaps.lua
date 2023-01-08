aeb0605c9de429554a41d79e62ce8e9a386245a5
Kevin Lei <kevvinlei89@gmail.com>
2023-01-08 18:04:45 -0500

    .

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,12 +1,14 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.number = true
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
+vim.opt.swapfile = false
 
 -- use spaces for tabs
 vim.opt.tabstop = 2
