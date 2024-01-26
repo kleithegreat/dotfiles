cb1b964d0ac1eb18d436429535bb624a12684550
kevin <kevvinlei89@gmail.com>
2024-01-26 17:43:25 -0600

    lmao

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,4 @@
 set fish_greeting
 starship init fish | source
 
-alias xneovide="WINIT_UNIX_BACKEND=x11 neovide"
 alias ls="exa -la"
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/nix/store/di47ia0jrm39s57d23qz6j4zq94liijk-micromamba-1.2.0/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/kevin/fish"
-$MAMBA_EXE shell hook --shell fish --prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
-function mamba
-    /nix/store/di47ia0jrm39s57d23qz6j4zq94liijk-micromamba-1.2.0/bin/micromamba $argv
-end
