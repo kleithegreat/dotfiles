731e4d6967eb5e44d92f509140cb10fb62646e2e
kevin <kevvinlei89@gmail.com>
2024-12-13 16:00:46 -0600

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ zoxide init fish | source
 thefuck --alias | source
 
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-alias cat="bat"
+# alias cat="bat"
 alias cd="z"
 alias skibidi="psql -h csce-315-db.engr.tamu.edu -U team_2b -d team_2b_db \ paras"
 
@@ -15,3 +15,28 @@ set PATH $PATH /home/kevin/.local/bin
 
 set -gx PATH /opt/cuda/bin $PATH
 set -gx LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/kevin/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/kevin/.opam/opam-init/init.fish' && source '/home/kevin/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
+
+# Nix
+if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+end
+
+# Add nix binaries to PATH
+set -gx PATH $PATH /nix/var/nix/profiles/default/bin
+set -x NIX_CONFIG "experimental-features = nix-command flakes"
