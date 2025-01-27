d8c4b85c0f2e9c8263a3d94c4f6cb01b88c09588
kevin <kevvinlei89@gmail.com>
2025-01-27 02:08:01 -0600

    doom emacs changes

--- fish/config.fish
+++ fish/config.fish
@@ -3,12 +3,9 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 
 starship init fish | source
 zoxide init fish | source
-thefuck --alias | source
 
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-# alias cat="bat"
 alias cd="z"
-alias skibidi="psql -h csce-315-db.engr.tamu.edu -U team_2b -d team_2b_db \ paras"
 
 # Created by `pipx` on 2024-05-28 18:58:49
 set PATH $PATH /home/kevin/.local/bin
@@ -22,15 +19,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/home/kevin/.opam/opam-init/init.fish' && source '/home/kevin/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
 
 # Nix
 if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
