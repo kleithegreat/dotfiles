50ad94aab50887428c4dc57c19f7e3532285eaae
kevin <kevvinlei89@gmail.com>
2025-01-31 01:33:33 -0600

    consolidate changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,9 @@
 -- Main entry point for Neovim configuration
 -- This file bootstraps lazy.nvim and loads other configuration files
 
+vim.cmd('cd /home/kevin/second-brain')
+vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+
 -- Set leader key to space (do this before lazy setup)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -24,3 +27,36 @@ require("options")  -- Load basic vim options and keymaps
 
 -- Initialize lazy.nvim with plugins
 require("lazy").setup("plugins")
+
+-- In init.lua or autocmd.lua
+vim.api.nvim_create_autocmd({"FileType"}, {
+  pattern = {"tex", "latex"},
+  callback = function()
+      -- Disable Treesitter for TeX files
+      vim.cmd('TSDisable highlight')
+      
+      -- Enable VimTeX syntax
+      vim.cmd('syntax enable')
+      
+      -- Configure concealment (optional)
+      vim.opt_local.conceallevel = 2
+      vim.opt_local.concealcursor = 'c'
+      
+      -- Other LaTeX-specific settings
+      vim.opt_local.spell = true
+      vim.opt_local.spelllang = "en_us"
+  end,
+})
+
+vim.g.neovide_scale_factor = 0.9
+
+-- Add word wrap specifically for Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+      vim.opt_local.wrap = true           -- Enable line wrapping
+      vim.opt_local.linebreak = true      -- Break lines at word boundaries
+      vim.opt_local.textwidth = 80        -- Set text width to 80 characters
+  end,
+  group = vim.api.nvim_create_augroup("markdown_settings", { clear = true })
+})
