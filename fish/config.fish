11057b3e98a087f10ef1a2d34b7ea42b46f1bd87
kevin <kevvinlei89@gmail.com>
2025-02-03 21:53:44 -0600

    init

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+set fish_greeting
+
+starship init fish | source
+zoxide init fish | source
+
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias cd="z"
+
+alias lmao="git add . && git commit -m "lmao" && git push"
+
+
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kevin/.ghcup/bin $PATH
+
+# Created by `pipx` on 2024-05-28 18:58:49
+set PATH $PATH /home/kevin/.local/bin
+
+set -gx PATH /opt/cuda/bin $PATH
+set -gx LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/kevin/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# Nix
+if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+end
+
+set -gx PATH $PATH /nix/var/nix/profiles/default/bin
+set -x NIX_CONFIG "experimental-features = nix-command flakes"
---
+++ old/nvim/lua/core/plugin_config/catppuccin.lua
@@ -0,0 +1,3 @@
+vim.o.termguicolors = true
+colorscheme = catppuccin
+vim.cmd.colorscheme "catppuccin"
