729b45ab376ecf2d889a7218a49550ef8d63ad53
kevin <kevvinlei89@gmail.com>
2026-03-01 17:39:24 -0600

    Phase C: home-manager + zsh + starship

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./system/configuration.nix
         ./hosts/vm/system.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.kevin = import ./home;
+        }
       ];
     };
   };
