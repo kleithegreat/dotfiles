024702d1193abe009aef0661e479eb10e6a5ad50
Kevin <kevvinlei89@gmail.com>
2026-04-10 00:39:24 -0500

    Fix Chromium font size conversion

---
+++ pkgs/helium/source.nix
@@ -0,0 +1,6 @@
+# Update by replacing `version` and recomputing the sha256 for
+# `https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64_linux.tar.xz`.
+{
+  version = "0.10.9.1";
+  sha256 = "1iyylx9d9hvqj8bnc7zqnkc6qijac0m16a8inq3vn8mq9x465gd1";
+}
